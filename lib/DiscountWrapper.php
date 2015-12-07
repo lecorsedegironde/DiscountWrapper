07beb718fbc2b69b04c5c9ec876aefffdce9744f
Pierre-Yves Le Guern <pierreyves.leguern3@gmail.com>
2015-12-08 00:24:22 +0100

    API non-bloquante : Resolved JSON Decode issue

--- lib/DiscountWrapper.php
+++ lib/DiscountWrapper.php
@@ -1,11 +1,71 @@
 <?php
 
 /**
- * User: @DopaSensei
- * Date: 07/12/2015
- * Time: 23:13
+ * Creator: @DopaSensei
+ *
+ * Creation Date: 07/12/2015
+ * Creation Time: 23:13
+ *
+ * Last Update Date: 08/12/2015
+ * Last Update Time: 00:23
  */
 class DiscountWrapper
 {
+    //Private API Key supplied by CDiscount
+    private $_key;
+
+    public function __construct($key){
+        $this->_key = $key;
+    }
+
+    //Get and decode the JSON file sent by the API through cURL. It doesn't uses SSL though.
+    private function getCurlFile($paramArray, $apiUrl){
+        $encodedJSON = json_encode($paramArray);
+        $ch = curl_init($apiUrl);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedJSON);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: '.strlen($encodedJSON)
+        ));
+
+        $res=curl_exec($ch);
+        curl_close($ch);
+
+        $answer = json_decode($res,true);
+        return $answer;
+    }
+
+    //Get CDiscount products without marketplace (TEST FUNCTION)
+    public function getProducts($productName){
+        $params = array(
+            "ApiKey" => $this->_key,
+            "SearchRequest" => array(
+                "Keyword" => $productName,
+                "SortBy" => "relevance",
+                "Pagination" => array(
+                    "ItemsPerPage" => 10,
+                    "PageNumber" => 0
+                ),
+                "Filters" => array(
+                    "Price" => array(
+                        "Min" => 0,
+                        "Max" => 400
+                    ),
+                    "Navigation" => "all",
+                    "IncludeMarketPlace" => true,
+                    "Condition" => "new"
+                )
+            ),
+        );
+
+        $relatedURL = "https://api.cdiscount.com/OpenApi/json/Search";
+
+        return json_decode(json_encode($this->getCurlFile($params,$relatedURL)),true);
+    }
 
 }
